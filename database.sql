6404d9be247e5343125e5c148c07c0f60d75e833
Michael Dunn <dunnmj42@gmail.com>
2021-02-07 13:34:09 -0600

    database.sql upd and .env

--- database.sql
+++ database.sql
@@ -4,7 +4,46 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial NOT NULL,
+	"username" varchar(255) NOT NULL UNIQUE,
+	"password" varchar(1000) NOT NULL,
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+CREATE TABLE "meals" (
+	"id" serial NOT NULL,
+	"name" varchar(255),
+	"description" TEXT,
+	"image" varchar(255),
+	"date" DATE,
+	"user_id" int,
+	CONSTRAINT "meals_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "ingredients" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"price" int NOT NULL,
+	CONSTRAINT "ingredients_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "meals_ingredients" (
+	"id" serial NOT NULL,
+	"ingredient_id" int NOT NULL,
+	"meal_id" int NOT NULL,
+	"ingredient_qty" int NOT NULL,
+	CONSTRAINT "meals_ingredients_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE "meals" ADD CONSTRAINT "meals_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "meals_ingredients" ADD CONSTRAINT "meals_ingredients_fk0" FOREIGN KEY ("ingredient_id") REFERENCES "ingredients"("id");
+ALTER TABLE "meals_ingredients" ADD CONSTRAINT "meals_ingredients_fk1" FOREIGN KEY ("meal_id") REFERENCES "meals"("id");
