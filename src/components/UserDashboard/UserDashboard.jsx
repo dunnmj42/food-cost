f7b0614e3bf368498fee38a98ef8fb3e5202338a
Michael Dunn <dunnmj42@gmail.com>
2021-02-09 13:10:26 -0600

    GET meals tested -- on DOM

--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function UserDashboard() {
   
   const user = useSelector((store) => store.user);
+  const meals = useSelector((store) => store?.meals);
 
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_MEALS'
+    })
+  }, [])
 
   return (
     <div className="container">
       <h1>User Dashboard</h1>
-      
+      {JSON.stringify(meals)}
     </div>
   );
 }
