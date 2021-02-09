5860db8bd8667f7cae2861e7e31f59b818ba4b17
Michael Dunn <dunnmj42@gmail.com>
2021-02-09 13:31:46 -0600

    userdash add meal and ACPM added non-funct

--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      display: 'flex',
+      flexWrap: 'wrap',
+      margin: 'auto',
+      justifyContent: 'center',
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
 function UserDashboard() {
   
   const user = useSelector((store) => store.user);
   const meals = useSelector((store) => store?.meals);
 
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,8 +38,16 @@ function UserDashboard() {
 
   return (
     <div className="container">
-      <h1>User Dashboard</h1>
-      {JSON.stringify(meals)}
+      <div className={classes.root}>
+      <Fab variant="extended" color="primary">
+        <AddIcon className={classes.extendedIcon} />
+        Add Meal
+      </Fab>
+      <br/>
+      <Paper>
+        Average Cost Per Meal: $9.99
+      </Paper>
+    </div>
     </div>
   );
 }
