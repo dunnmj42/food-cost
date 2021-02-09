c5bd2a1206b6c843ba1c90002cf3b51c4dca8dd3
Michael Dunn <dunnmj42@gmail.com>
2021-02-09 17:14:37 -0600

    Nav cleanup

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 // app bar imports
