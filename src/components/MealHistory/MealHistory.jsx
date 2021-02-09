ee7bfe9e0d84dfb5c41cba551179d56baa000b20
Michael Dunn <dunnmj42@gmail.com>
2021-02-09 17:41:11 -0600

    mealhistory mapping, calls mealcard

--- src/components/MealHistory/MealHistory.jsx
+++ src/components/MealHistory/MealHistory.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import Grid from '@material-ui/core/Grid';
+
+import MealCard from "../MealCard/MealCard";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: "auto",
+  },
+}));
 
 function MealHistory() {
+
+  const meals = useSelector((store) => store?.meals);
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_MEALS",
+    });
+  }, []);
+
   return (
-    <div className="container">
-      <p>Meal History</p>
+    <div>
+      <Grid container spacing={3} className={classes.root}>
+        {meals?.map((meal, i) => {
+          return (
+            <Grid item m={6} key={i}>
+              <MealCard meal={meal} />
+            </Grid>
+          )
+        })}
+      </Grid>
     </div>
   );
 }
