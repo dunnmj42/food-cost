16129cd8a8feb76bbc7c58ccf17c5f8c6a4d21df
Michael Dunn <dunnmj42@gmail.com>
2021-02-09 15:14:31 -0600

    mealcard componentize

---
+++ src/components/MealCard/MealCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  mealcard: {
+    maxWidth: 345,
+    justifyContent: "left",
+  },
+}));
+
+function MealCard({ meal }) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Card className={classes.mealcard} key={meal?.id}>
+        <CardHeader
+          title={meal?.name}
+          subheader={new Date(meal?.date).toLocaleDateString("en-us")}
+        />
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={meal?.name}
+            height="180"
+            image={meal?.image}
+            title={meal?.name}
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {meal?.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Details
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+}
+
+export default MealCard;
--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -10,15 +10,7 @@ import AddIcon from "@material-ui/icons/Add";
 // paper for A-CPM
 import Paper from "@material-ui/core/Paper";
 
-// card imports - to breakout
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import MealCard from "../MealCard/MealCard";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,17 +28,12 @@ const useStyles = makeStyles((theme) => ({
     margin: 40,
     padding: 5,
   },
-  mealcard: {
-    maxWidth: 345,
-    justifyContent: "left",
-  },
 }));
 
 function UserDashboard() {
-  const user = useSelector((store) => store.user);
   const meals = useSelector((store) => store?.meals);
 
-  const recentMeal = meals[0];
+  const meal = meals[0];
 
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -65,31 +52,7 @@ function UserDashboard() {
           Add Meal
         </Fab>
         <Paper className={classes.cpm}>Average Cost Per Meal: $9.99</Paper>
-        <Card className={classes.mealcard} key={recentMeal?.id}>
-          <CardHeader
-            title={recentMeal?.name}
-            subheader={new Date(recentMeal?.date).toLocaleDateString("en-us")}
-          />
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt={recentMeal?.name}
-              height="180"
-              image={recentMeal?.image}
-              title={recentMeal?.name}
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {recentMeal?.description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Details
-            </Button>
-          </CardActions>
-        </Card>
+        <MealCard meal={meal}/>
       </div>
     </div>
   );
