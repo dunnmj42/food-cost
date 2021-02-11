f5f01d2a546166fa7756cc31a3c9c1d59d76a01b
Michael Dunn <dunnmj42@gmail.com>
2021-02-11 09:48:56 -0600

    sql update > m-m === o-m. drop mealsingredients

--- database.sql
+++ database.sql
@@ -12,6 +12,8 @@ CREATE TABLE "user" (
   OIDS=FALSE
 );
 
+
+
 CREATE TABLE "meals" (
 	"id" serial NOT NULL,
 	"name" varchar(255),
@@ -19,32 +21,28 @@ CREATE TABLE "meals" (
 	"image" varchar(255),
 	"date" DATE,
 	"user_id" int,
-	"portions" int,
+	"portions" int NOT NULL,
 	CONSTRAINT "meals_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
+
+
 CREATE TABLE "ingredients" (
 	"id" serial NOT NULL,
 	"name" varchar(255) NOT NULL,
-	"price" real NOT NULL,
+	"price" int NOT NULL,
+	"ingredient_qty" real NOT NULL,
+	"meal_id" int NOT NULL,
 	CONSTRAINT "ingredients_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE "meals_ingredients" (
-	"id" serial NOT NULL,
-	"ingredient_id" int NOT NULL,
-	"meal_id" int NOT NULL,
-	"ingredient_qty" real NOT NULL,
-	CONSTRAINT "meals_ingredients_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
+
+
 
 ALTER TABLE "meals" ADD CONSTRAINT "meals_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
-ALTER TABLE "meals_ingredients" ADD CONSTRAINT "meals_ingredients_fk0" FOREIGN KEY ("ingredient_id") REFERENCES "ingredients"("id");
-ALTER TABLE "meals_ingredients" ADD CONSTRAINT "meals_ingredients_fk1" FOREIGN KEY ("meal_id") REFERENCES "meals"("id");
+ALTER TABLE "ingredients" ADD CONSTRAINT "ingredients_fk0" FOREIGN KEY ("meal_id") REFERENCES "meals"("id");
