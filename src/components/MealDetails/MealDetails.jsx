c496b8460641ed2609b3cc74a1daed9f661e3535
Michael Dunn <dunnmj42@gmail.com>
2021-02-15 15:12:15 -0600

    details layout

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -6,9 +6,9 @@ import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Paper from "@material-ui/core/Paper";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import InputAdornment from '@material-ui/core/InputAdornment';
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 import MealCard from "../MealCard/MealCard";
 
@@ -26,9 +26,6 @@ const useStyles = makeStyles((theme) => ({
     margin: 40,
     padding: 5,
   },
-  fieldWidth: {
-    width: 25,
-  },
 }));
 
 function MealDetails() {
@@ -44,61 +41,72 @@ function MealDetails() {
   const buttonTitle = "EDIT";
 
   const handleClick = () => {
-    dispatch({ type: "FETCH_DETAILS", payload: meal?.id }); 
-    history.push("/edit"); 
+    dispatch({ type: "FETCH_DETAILS", payload: meal?.id });
+    history.push("/edit");
   };
 
   return (
     <div className="container">
       <div className={classes.root}>
-      <div>
-      <MealCard meal={meal} handleClick={handleClick} buttonTitle={buttonTitle}/>
-      </div>
-      <div>
-      <Paper className={classes.cpm} onClick={() => history.push("/trends")}>
-        Cost Per Meal: ${meal?.cost_per_meal.toFixed(2)}
-      </Paper>
-      </div>
-      <div>
-      <Grid container spacing={2} className={classes.root}>
-        {ingredients?.map((ingredient, i) => {
-          return (
-            <Grid container spacing={2} key={i}>
-              <Grid item>
-              <TextField
-                label="Ingredient"
-                variant="outlined"
-                value={ingredient?.name}
-              />
-              </Grid>
-              <Grid item>
-              <TextField
-                label="Price"
-                variant="outlined"
-                InputProps={ {startAdornment: <InputAdornment position="start">$</InputAdornment>}}
-                value={ingredient?.price}
-              />
-              </Grid>
-              <Grid item>
-              <TextField
-                label="Quantity Used"
-                variant="outlined"
-                value={ingredient?.ingredient_qty}
-              />
-              </Grid>
-            </Grid>
-          );
-        })}
-        <Grid item>
-          <TextField
-            label="Portions Made"
-            InputLabelProps={{ shrink: true }}
-            variant="outlined"
-            value={meal?.portions}
+        <div>
+          <MealCard
+            meal={meal}
+            handleClick={handleClick}
+            buttonTitle={buttonTitle}
           />
-        </Grid>
-      </Grid>
-      </div>
+        </div>
+        <div>
+          <Paper
+            className={classes.cpm}
+            onClick={() => history.push("/trends")}
+          >
+            Cost Per Meal: ${meal?.cost_per_meal.toFixed(2)}
+          </Paper>
+        </div>
+        <div>
+          <Grid container spacing={3} className={classes.root}>
+            {ingredients?.map((ingredient, i) => {
+              return (
+                <Grid container spacing={1} key={i}>
+                  <Grid item xs={6} lg={2}>
+                    <TextField
+                      label="Ingredient"
+                      variant="outlined"
+                      value={ingredient?.name}
+                    />
+                  </Grid>
+                  <Grid item xs={3} lg={2}>
+                    <TextField
+                      label="Price"
+                      variant="outlined"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">$</InputAdornment>
+                        ),
+                      }}
+                      value={ingredient?.price}
+                    />
+                  </Grid>
+                  <Grid item xs={3} lg={2}>
+                    <TextField
+                      label="Quantity Used"
+                      variant="outlined"
+                      value={ingredient?.ingredient_qty}
+                    />
+                  </Grid>
+                </Grid>
+              );
+            })}
+            <Grid item>
+              <TextField
+                label="Portions Made"
+                InputLabelProps={{ shrink: true }}
+                variant="outlined"
+                value={meal?.portions}
+              />
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </div>
   );
