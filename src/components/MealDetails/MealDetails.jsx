49b9cbb174d77e28957c21abad78cb0211dd7036
Michael Dunn <dunnmj42@gmail.com>
2021-02-15 13:11:28 -0600

    detail view cleanup

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -5,19 +5,10 @@ import { useHistory } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-
 import Paper from "@material-ui/core/Paper";
 
 import TextField from "@material-ui/core/TextField";
 
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
 import MealCard from "../MealCard/MealCard";
 
 const useStyles = makeStyles((theme) => ({
@@ -41,10 +32,6 @@ function MealDetails() {
   const dispatch = useDispatch();
 
   const details = useSelector((store) => store?.details);
-
-  // useEffect(() => {
-  //   dispatch({ type: "FETCH_DETAILS", payload: meal?.id });
-  // }, []);
 
   const meal = details[0];
   const ingredients = details[1];
