9e8ea61c5a896cdbd62be294c119b39fb707c43b
Michael Dunn <dunnmj42@gmail.com>
2021-02-16 15:57:12 -0600

    landing update and css removal

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,39 +1,40 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LandingPage.css';
+import React, { useState } from "react";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 // CUSTOM COMPONENTS
-import LoginDialog from '../LoginDialog/LoginDialog';
-import RegisterDialog from '../RegisterDialog/RegisterDialog';
+import LoginDialog from "../LoginDialog/LoginDialog";
+import RegisterDialog from "../RegisterDialog/RegisterDialog";
 
 function LandingPage() {
   const [login, setLogin] = useState(false);
   const [register, setRegister] = useState(false);
-  
+
   const handleLoginClick = () => {
     setLogin(true);
   };
 
   const handleRegisterClick = () => {
     setRegister(true);
-  }
-  
+  };
 
   return (
     <div className="container">
-          <center>
-          <h1>Welcome</h1>
-            <h4>New User?</h4>
-            <Button variant="outlined" color="primary" onClick={handleRegisterClick}>
-              Register
-            </Button>
-            <h4>Already Have an Account?</h4>
-            <Button variant="outlined" color="primary" onClick={handleLoginClick}>
-              Login
-            </Button>
-          </center>
+      <center>
+        <h1>Welcome</h1>
+        <h4>New User?</h4>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleRegisterClick}
+        >
+          Register
+        </Button>
+        <h4>Already Have an Account?</h4>
+        <Button variant="outlined" color="primary" onClick={handleLoginClick}>
+          Login
+        </Button>
+      </center>
       <RegisterDialog register={register} setRegister={setRegister} />
       <LoginDialog login={login} setLogin={setLogin} />
     </div>
