c196c11ab5280d4b26a42fc5631c44aa1a7083b3
Michael Dunn <dunnmj42@gmail.com>
2021-02-17 12:08:25 -0600

    chart color

--- src/components/TrendChart/TrendChart.jsx
+++ src/components/TrendChart/TrendChart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import { Line } from "react-chartjs-2";
 
@@ -16,9 +16,9 @@ function TrendChart({meals}) {
     const ctx = canvas?.getContext('2d');
     const gradient = ctx?.createLinearGradient(0, 0, 0, 140)
 
-    gradient?.addColorStop(0, "rgba(173, 53, 186, 0.5)");
+    gradient?.addColorStop(0, "rgba(63, 81, 181, 0.5)");
 
-    gradient?.addColorStop(1, "rgba(173, 53, 186, 0.1)");
+    gradient?.addColorStop(1, "rgba(63, 81, 181, 0.1)");
     
     return {
       labels: labels,
@@ -28,7 +28,7 @@ function TrendChart({meals}) {
           data: values,
           backgroundColor: gradient,
           borderColor: [
-            '#AD35BA',
+            '#3f51b5',
           ],
           borderWidth: 1,
           pointBorderColor: "#888",
@@ -37,6 +37,8 @@ function TrendChart({meals}) {
       ],
     };
   };
+
+  Chart.defaults.global.legend.display = false;
 
   useEffect(() => {
     chart();
